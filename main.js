c9cb09ed5cf6dbe2e9a83d1b6584172f1bfd2e0f
Alice Peruniz <apo2@cin.ufpe.br>
2022-11-29 21:50:14 -0300

    adicionando comentários

--- main.js
+++ main.js
@@ -1,20 +1,22 @@
+/* Definido as constantes do personagem do jogo(chamando pelas classes das divs no HTML) */
 const character = document.getElementsByClassName("character")[0];
 const containerCharacter = document.getElementsByClassName("container-character")[0];
 
-
+/* Definindo a constante da velocidade de deslocamento do personagem */
 const VELOCITY = 10;
 
-const SCREEN_WIDTH = screen.width;
-const SCREEN_HEIGHT = screen.height;
+const SCREEN_WIDTH = screen.width;  /* Constante da largura da tela */
+const SCREEN_HEIGHT = screen.height;  /* Constante da altura da tela */
 
-let num = screen.width - 100
-let num_h = screen.height - 170
+let num = screen.width - 110 /* Largura máxima permitida para o descolamento do personagem */
+let num_h = screen.height - 180 /* Altura máxima permitida para o deslocamento do personagem */
 
+/* Posição inicial do personagem: */
 let xPosition = 500;
 let yPosition = 300;
 
-const keysAvaiable = ["ArrowUp", "ArrowDown", "ArrowLeft", "ArrowRight"]
-const directions = ["turnUp", "turnLeft", "turnRight", "turnDown"];
+const keysAvaiable = ["ArrowUp", "ArrowDown", "ArrowLeft", "ArrowRight"] /* Definindo quais teclas terão funções no jogo */
+const directions = ["turnUp", "turnLeft", "turnRight", "turnDown"]; /* Direções que o personagem vai poder seguir */
 
 window.addEventListener("keydown", (event) => {
     const key  = event.key;
